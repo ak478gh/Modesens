cb18693bb2e4c4879ec5a58011f930630b5b4a74
AmitKumar <hackak4444@gmail.com>
2022-10-02 11:30:57 +0530

    add the remove functionality in tht cart page

--- css.js
+++ css.js
@@ -2,17 +2,18 @@
     let cartItemFromLS = JSON.parse(localStorage.getItem('cart_items')) ||[];
     // console.log(cartItemFromLS)
 
-    // Calculating the prince of total items.
-    // let totalPrice = cartItemFromLS.reduce(function(acc,el) {
-    //     return acc + +el.price;
-    // },0);
-    // document.querySelector('h2>span').innerText = totalPrice;
+    // Calculating the price of total items.
+    let totalPrice = cartItemFromLS.reduce(function(acc,el) {
+        return acc + +el.price;
+    },0);
+    // Calculating the total price.
+    document.querySelector('#kharch').innerText = '$ '+totalPrice;
 
     // Calculating the number of total items.
-    // document.querySelector('h1>span').innerText = cartItemFromLS.length;
+    document.querySelector('#counter').innerText = cartItemFromLS.length;
 
     // Creating the card div
-    cartItemFromLS.forEach(function(elem){
+    cartItemFromLS.forEach(function(elem, i){
         let card = document.createElement('div');
         
         let itemImg = document.createElement('img'); // Creating the image tag
@@ -27,16 +28,30 @@
         let price = document.createElement('p'); // Creating the p tag
         price.innerText = elem.price;
 
+        let desc = document.createElement('div')
+        desc.setAttribute('id', 'itemDesc')
+
+        let itemPrice = document.createElement('div')
+        itemPrice.setAttribute('id', 'itemPrice')
+
         let remove = document.createElement('button');
         remove.innerText = 'REMOVE'
-        document.querySelector('#cards').append(card); // Appending the card in container
-        card.append(itemImg, itemName, itemDesc, price, remove); // Appending the tags in card
+        
+        desc.append(itemName, itemDesc)
+        itemPrice.append(price, remove)
+        card.append(itemImg, desc, itemPrice); // Appending the tags in card
+
+        document.querySelector('#cards').append(card); // Appending the card in container;
 
         remove.addEventListener('click', function(elem) {
-            console.log(elem.target.parentNode.remove());
+            console.log(elem.target.parentNode.parentNode.remove());
+            deleteItem(i);
         })
     });
 
-    // function removeItem(elem) {
-    //     console.log(elem.)
-    // }
+    function deleteItem(i) {
+        cartItemFromLS.splice(i, 1);
+        // console.log(cartItemFromLS)
+        localStorage.setItem('cart_items', JSON.stringify(cartItemFromLS)) // Updating the items in LS
+        window.location.reload(); // Reloading the Page
+    }
